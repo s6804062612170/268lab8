7a379a0700813c18f926214a702e151cc9b03aa8
s6804062612170 <s6804062612170@email.kmutnb.ac.th>
2026-02-24 12:51:34 +0700

    Commit 6 >> solution2

--- lab8no4.cpp
+++ lab8no4.cpp
@@ -1,28 +1,23 @@
 #include <stdio.h>
 
 int main() {
+
     int score[4] = {80,65,90,70};
-    int best[3];
-    int i, j, max, maxIndex;
+    int i, j, temp;
 
-    for(i=0;i<3;i++) {
-
-        max = score[0];
-        maxIndex = 0;
-
-        for(j=1;j<4;j++){
-            if(score[j] > max){
-                max = score[j];
-                maxIndex = j;
+    for(i = 0; i < 4-1; i++) {
+        for(j = 0; j < 4-i-1; j++) {
+            if(score[j] < score[j+1]) {
+                temp = score[j];
+                score[j] = score[j+1];
+                score[j+1] = temp;
             }
         }
-        best[i] = max;
-        score[maxIndex] = -1;
     }
 
-    printf("Best 3 scores:\n");
-    for(i=0;i<3;i++)
-        printf("%d ", best[i]);
+    printf("Best 3 scores (Method 2):\n");
+    for(i = 0; i < 3; i++)
+        printf("%d ", score[i]);
 
     return 0;
 }
