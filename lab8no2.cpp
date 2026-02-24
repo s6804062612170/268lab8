7a356da278c1e8ab6fc7f90b522489b9143e51b7
s6804062612170 <s6804062612170@email.kmutnb.ac.th>
2026-02-24 12:42:16 +0700

    Commit 2 >> Lab8no2.c: show all score

---
+++ lab8no2.cpp
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+int checkscore(char std[]);
+
+int main(){
+	int i,j;
+	char ans[8][10] ={
+	{'A','B','A','C','C','D','E','E','A','D'},//7
+	{'D','B','A','B','C','A','E','E','A','D'},//6
+	{'E','D','D','A','C','B','E','E','A','D'},//5
+	{'C','B','A','E','D','C','E','E','A','D'},//4
+	{'A','B','D','C','C','D','E','E','A','D'},//8
+	{'B','B','E','C','C','D','E','E','A','D'},//7
+	{'B','B','A','C','C','D','E','E','A','D'},//7
+	{'E','B','E','C','C','D','E','E','A','D'}};//7\
+	charkeys[10] ={'D','B','D','C','C','D','A','E','A','D'};
+	for (i=0;i<8;i++){
+		printf("std %d => %d\n",(i+1),checkscore(ans[i]));
+	}
+	
+}
+int checkscore(char std[]){
+	int i;
+	int score = 0;
+	char charkeys[10] = {'D','B','D','C','C','D','A','E','A','D'};
+	
+	for (i=0;i<10;i++){
+		if(std[i] == charkeys[i])
+			score++;
+	}
+	return score;
+}
